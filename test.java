3c5be34610c5cbe1adac2db2c27cac3f182137a8
MofThabang <4362668@myuwc.ac.za>
2026-02-25 14:48:05 +0200

    Create test.java
    int key and data

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.lang.math;
+import java.io;
+import java.text.
+
+  class Node{
+    int key;
+    String data;
+    Node( int k, string d){
+      key=k
+      data=d
+
+        public class timeMethods{
+        public static void main(String[]args){
+          
