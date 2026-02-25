530c83511796a1f3a2e1426f750d865685d3f21f
MofThabang <4362668@myuwc.ac.za>
2026-02-25 13:13:52 +0000

    linear and binary time(S) added

--- test.java
+++ test.java
@@ -8,9 +8,19 @@ import java.text.
     Node( int k, string d){
       key=k
       data=d
+    }
 
         public class timeMethods{
+        public static int N =32654  
         public static void main(String[]args){
-          public static int N =326554  // actual size
+          decimalFormat twoD = new decimalFormat("0.00")
+          decimalFormat fourD= new decimalFormat("0.0000")
+          decimalFormat  fiveD= new decimalFormat("0.00000")
+        }
+        double linearTime =0,linearTime2 =0;
+        double binaryTime =0,binaryTime2 =0;
+        double TotalTime =0;
+        
           
+
           
