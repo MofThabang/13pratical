109382880107bf451566b196a5b5a078e146d1ba
MofThabang <4362668@myuwc.ac.za>
2026-02-25 12:57:02 +0000

    added actual size of N=32654

--- test.java
+++ test.java
@@ -11,5 +11,6 @@ import java.text.
 
         public class timeMethods{
         public static void main(String[]args){
+          public static int N =326554  // actual size
           
-// fjhsdkj
+          
